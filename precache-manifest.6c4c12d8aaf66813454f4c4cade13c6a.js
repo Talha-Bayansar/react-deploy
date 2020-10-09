856e49c8f5f3393aa623b0a3e5d7b0185e354d5a
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-09 12:04:57 +0200

    Updates

---
+++ precache-manifest.6c4c12d8aaf66813454f4c4cade13c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f007f136a9656ca53c494918b37270d5",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "daf0fafb03e6d2afcbc8",
+    "url": "/react-deploy/static/css/main.e2b28370.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf0fafb03e6d2afcbc8",
+    "url": "/react-deploy/static/js/main.40eb6b61.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  }
+]);

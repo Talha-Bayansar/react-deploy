760da0226e5698b0d3753478687c8437f90fc81a
Talha-Bayansar <talha.bayansar@hotmail.com>
2020-10-16 11:29:12 +0200

    add environment configurations

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 
 function Banner() {
+  const [message, setMessage] = useState(process.env.REACT_APP_MESSAGE);
   return (
     <div className="banner">
       <p className="banner-text">
         This is some text above the Call To Action Button.
       </p>
       <button className="banner-ctaButton">CLICK TO SUBSCRIBE</button>
+      <h1>{message}</h1>
     </div>
   );
 }
